107130825a342c3f198b53d09d1912d6e01db9f2
Kyle Oba <koba@mudphone.com>
2014-04-22 23:24:48 -1000

    Add basic square root example.

--- src/sicp/lecture1a.clj
+++ src/sicp/lecture1a.clj
@@ -6,6 +6,25 @@
   (* x x))
 
 (defn abs [x]
+  "Absolute value of number"
   (if (< x 0)
     (- x)
     x))
+
+(defn average [& vec]
+  (cond
+   (empty? vec) nil
+   :else (float (/ (apply + vec) (count vec)))))
+
+(defn sqrt [x]
+  "Square root of number"
+  (let [improve (fn [guess]
+                  (average guess (/ x guess)))
+        good-enough? (fn [guess]
+                       (< (abs (- (square guess) x))
+                          0.001))
+        try-guess (fn [guess]
+              (if (good-enough? guess)
+                guess
+                (recur (improve guess))))]
+    (try-guess 1)))
--- test/sicp/lecture1a_test.clj
+++ test/sicp/lecture1a_test.clj
@@ -12,4 +12,18 @@
     (is (= 4 (abs 4)))
     (is (= 0 (abs 0)))))
 
+(deftest average-test
+  (testing "caluclates an average of the given numbers"
+    (is (= 2.0 (average 2 2 2)))
+    (is (nil? (average)))
+    (is (= 3.5 (average 5 2)))))
 
+(defn round [x]
+  (read-string (format "%.3f" x)))
+
+(deftest sqrt-test
+  (testing "square root of num"
+    (is (= 2.0 (round (sqrt 4))))
+    (is (= 3.0 (round (sqrt 9))))
+    (is (= 5.0 (round (sqrt 25))))
+    (is (= 10.0 (round (sqrt 100))))))
