712651ad646cf13abc14934e4f0d73d7a340dbe0
Kyle Oba <koba@mudphone.com>
2014-04-26 02:33:02 -1000

    Add higher order method for finding sums.

--- src/sicp/lecture2a.clj
+++ src/sicp/lecture2a.clj
@@ -28,3 +28,17 @@
     0
     (float (+ (/ 1 (* a (+ a 2)))
               (pi-sum (+ a 4) b)))))
+
+(defn sum
+  "Generic sum-er"
+  [term-f a next-f b]
+  (if (> a b)
+    0
+    (+ (term-f a)
+       (sum term-f (next-f a) next-f b))))
+
+(defn sum-int2
+  "Alternative sum of integers"
+  [a b]
+  (let [identity (fn [x] x)]
+    (sum identity a inc b)))
--- test/sicp/lecture2a_test.clj
+++ test/sicp/lecture2a_test.clj
@@ -13,3 +13,13 @@
 (deftest sum-sq-test
   (testing "sum of squares"
     (is (= 25 (sum-sq 3 4)))))
+
+(deftest pi-sum-test
+  (testing "Leibnitz's formula"
+    (let [pi_8 (/ 3.14159 8.0)]
+      (is (> 0.001 (- pi_8 (pi-sum 1 1000)))))))
+
+(deftest sum-int2-test
+  (testing "more general solution for sum of integers"
+    (is (= 15 (sum-int2 7 8)))
+    (is (= 12 (sum-int2 3 5)))))
