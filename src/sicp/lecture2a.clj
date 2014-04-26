061e58de69562fb3e648631a2e730abe3a6469f2
Kyle Oba <koba@mudphone.com>
2014-04-26 01:59:52 -1000

    Add basic sum of squares.

--- src/sicp/lecture2a.clj
+++ src/sicp/lecture2a.clj
@@ -7,3 +7,16 @@
     0
     (+ a
        (sum-int (inc a) b))))
+
+(defn square
+  "Square of num"
+  [a]
+  (* a a))
+
+(defn sum-sq
+  "Sum of squares of a to b"
+  [a b]
+  (if (> a b)
+    0
+    (+ (square a)
+       (sum-sq (inc a) b))))
--- test/sicp/lecture2a_test.clj
+++ test/sicp/lecture2a_test.clj
@@ -5,3 +5,11 @@
 (deftest sum-int-test
   (testing "sum of integers"
     (is (= 15 (sum-int 7 8)))))
+
+(deftest square-test
+  (testing "square again"
+    (is (= 9 (square 3)))))
+
+(deftest sum-sq-test
+  (testing "sum of squares"
+    (is (= 25 (sum-sq 3 4)))))
