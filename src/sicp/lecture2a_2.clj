aac8cf61cba448e59bb6e372b7b25803aee21b03
Kyle Oba <koba@mudphone.com>
2014-05-04 09:15:51 -1000

    Add two square root methods describing Heron of Alexandria's method.

--- src/sicp/lecture2a_2.clj
+++ src/sicp/lecture2a_2.clj
@@ -1,7 +1,42 @@
-(ns sicp.lecture2a-2)
+(ns sicp.lecture2a-2
+  (:require [sicp.lecture1a :refer [abs average]]))
 
 ;; Higher-order Procedures
 ;; Lecture 2A
 ;; Part 2 - Text Section 1.3
 ;; http://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-001-structure-and-interpretation-of-computer-programs-spring-2005/video-lectures/2a-higher-order-procedures/
 
+(defn fixed-point
+  "Find fixed point of the procedure whose name will be f in
+   this procedure"
+  [f start]
+  (let [tolerance 0.00001
+        close-enuf? (fn [u v]
+                      (< (abs (- u v)) tolerance))
+        iter (fn [old new]
+               (if (close-enuf? old new)
+                 new
+                 (recur new (f new))))]
+    (iter start (f start))))
+
+(defn sqrt
+  "Square root via Fixed-Point"
+  [x]
+  (fixed-point (fn [y]
+                 (average (/ x y) y))
+               1))
+
+(defn average-damp
+  "A procedure which takes a procedure and returns a procedure
+   which averages the application with the input."
+  [f]
+  (fn [x]
+    (average (f x) x)))
+
+(defn sqrt-avg-damp
+  "Square root via average-damping method"
+  [x]
+  (fixed-point
+   (average-damp (fn [y]
+                   (/ x y)))
+   1))
--- test/sicp/lecture2a_2_test.clj
+++ test/sicp/lecture2a_2_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [sicp.lecture2a-2 :refer :all]))
 
-_#(deftest sum-int-test
-  (testing "sum of integers"
-    (is (= 15 (sum-int 7 8)))))
+(deftest sqrt-test
+  (testing "sqrt via heron method"
+    (is (= 2.0 (sqrt 4)))))
+
+(deftest sqrt-avg-damp-test
+  (testing "sqrt via average damping simplification"
+    (is (= 2.0 (sqrt-avg-damp 4)))))
