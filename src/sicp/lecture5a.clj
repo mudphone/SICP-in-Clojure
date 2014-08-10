075fd3f37bc0a5e1789145512d05af1be2c54eaa
Kyle Oba <koba@mudphone.com>
2014-08-10 08:16:14 -1000

    Preparing to start work on lecture 5a.

---
+++ src/sicp/lecture5a.clj
@@ -0,0 +1,30 @@
+(ns sicp.lecture5a)
+
+;; Assignment, State & Side-effects
+;; Lecture 5A
+;; Text Section 3.1 & 3.2
+;; https://www.youtube.com/watch?v=jl8EHP1WrWY&index=9&list=PLB63C06FAF154F047
+
+;; Part 1 - 0:00
+;; Functional and imperative factorial implementations
+
+;; Part 3 - 47:16
+;; Cesaro's method for estimating Pi
+
+;; 1:08:15
+(defn my-rand [])
+
+;; 1:06:44
+(defn monte-carlo [trials experiment])
+
+;; 1:03:54
+(defn gcd [x y])
+(defn estimate-pi [n])
+(defn cesaro [])
+
+
+;;
+;; 1:09:56 estimate-pi without state
+;; 1:10:47
+;; 1:10:58
+;; 1:11:51
---
+++ test/sicp/lecture5a_test.clj
@@ -0,0 +1,7 @@
+(ns sicp.lecture5a-test
+  (:require [clojure.test :refer :all]
+            [sicp.lecture5a :refer :all]))
+
+(deftest x-test
+  (testing "something"
+    (is (= 1 1) "Simple addition")))
